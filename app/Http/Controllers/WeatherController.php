177784ff69ef266c4faf646b2b8add404ddc9437
Christian Ray Garcia <crgarcia@johnsonsberkshire.com>
2024-10-01 13:44:53 +0800

    backend changes

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
@@ -10,9 +11,19 @@ class WeatherController extends Controller
     {
         $apiKey = '414462081ea0e3ee3afcd78b6615ceea';
         $url = "https://api.openweathermap.org/data/2.5/forecast?q={$city},JP&appid={$apiKey}";
-    
-        $response = file_get_contents($url);
-        return response()->json(json_decode($response));
+
+        $response = Http::get($url);
+
+        if ($response->failed()) {
+            return response()->json(['error' => 'Error fetching weather data'], 500);
+        }
+
+        $weather = $response->json();
+
+        // dd($weather);
+
+        return response()->json($weather);
+        
     }
     
 }
